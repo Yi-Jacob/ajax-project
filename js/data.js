4943a5de9f71a9bcef0a86c61fda062a7ccea632
jacobyi11 <jakeyi00@gmail.com>
2022-03-02 19:44:27 +0000

    working on javascript portion

--- js/data.js
+++ js/data.js
@@ -1,1 +1,22 @@
 /* exported data */
+
+var data = {
+  results: [],
+  moreInfo: null,
+  view: '',
+  searchPageView: '',
+  bookmarks: []
+};
+
+var previousResultsJSON = localStorage.getItem('results-list');
+
+if (previousResultsJSON !== null) {
+  data = JSON.parse(previousResultsJSON);
+}
+
+function handleUnload(event) {
+  var resultsJSON = JSON.stringify(data);
+  localStorage.setItem('results-list', resultsJSON);
+}
+
+window.addEventListener('beforeunload', handleUnload);
