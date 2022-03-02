2735f65b9d62e9f20090acbbef3cb25c2f247887
jacobyi11 <jakeyi00@gmail.com>
2022-03-02 21:11:08 +0000

    added event listener

--- js/data.js
+++ js/data.js
@@ -1,11 +1,33 @@
 /* exported data */
 
+// var data = {
+//   results: [],
+//   moreInfo: null,
+//   view: '',
+//   searchPageView: '',
+//   bookmarks: []
+// };
+
+// var previousResultsJSON = localStorage.getItem('results-list');
+
+// if (previousResultsJSON !== null) {
+//   data = JSON.parse(previousResultsJSON);
+// }
+
+// function handleUnload(event) {
+//   var resultsJSON = JSON.stringify(data);
+//   localStorage.setItem('results-list', resultsJSON);
+// }
+
+// window.addEventListener('beforeunload', handleUnload);
 var data = {
+  bookmarks: [],
   results: [],
-  moreInfo: null,
+  detail: null,
+  removeId: 0,
+  nextResultId: 0,
   view: '',
-  searchPageView: '',
-  bookmarks: []
+  searchPageView: ''
 };
 
 var previousResultsJSON = localStorage.getItem('results-list');
