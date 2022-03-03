2605d3ef759aed605db5410d0884e4de1b0cbb44
jacobyi11 <jakeyi00@gmail.com>
2022-03-03 00:35:37 +0000

    with preventDefault method

--- js/main.js
+++ js/main.js
@@ -5,13 +5,10 @@ var $searchForm = document.getElementById('search-form');
 $submitButton.addEventListener('click', getResults);
 
 function getResults(event) {
-
+  event.preventDefault();
   var inputValue = $searchInput.value;
-
   var xhr = new XMLHttpRequest();
-
   xhr.open('GET', 'https://api.openbrewerydb.org/breweries?by_city=' + inputValue);
-
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
 
